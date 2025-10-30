a54b2f88d48c890e0d702fc5aecee7fde11258d2
Gustavo Gomes <devgustavogomes31@gmail.com>
2025-10-30 13:56:05 -0300

    zod schema `producers` fixed

--- backendnest/src/producer/dto/producerInput.dto.ts
+++ backendnest/src/producer/dto/producerInput.dto.ts
@@ -2,17 +2,35 @@ import z from 'zod';
 
 export const createProducerSchema = z.object({
   name: z.string().min(5).max(128).trim(),
-  CPForCNPJ: z.string().trim(),
-  password: z.string().trim(),
+  CPForCNPJ: z.preprocess(
+    (val) => {
+      if (typeof val !== 'string') return val;
+
+      return val.replace(/\D/g, '');
+    },
+    z.string().regex(/^(?:\d{11}|\d{14})$/, 'Invalid CPF or CNPJ'),
+  ),
+  password: z
+    .string()
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{8,36}$/)
+    .trim(),
 });
 export type CreateProducerInput = z.infer<typeof createProducerSchema>;
+
 export type CreateProducerDTO = Omit<CreateProducerInput, 'password'> & {
   hashedPassword: string;
 };
 
 export const changeProducerSchema = z.object({
   name: z.string().min(5).max(128).trim(),
-  CPForCNPJ: z.string(),
+  CPForCNPJ: z.preprocess(
+    (val) => {
+      if (typeof val !== 'string') return val;
+
+      return val.replace(/\D/g, '');
+    },
+    z.string().regex(/^(?:\d{11}|\d{14})$/, 'Invalid CPF or CNPJ'),
+  ),
 });
 
 export type changeProducerDTO = z.infer<typeof changeProducerSchema>;
