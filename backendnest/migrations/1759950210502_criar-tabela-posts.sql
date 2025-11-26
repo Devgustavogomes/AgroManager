14d1f305a49eea78e5cb05e1d571bb384018ab30
DevGustavo <devgustavogomes31@gmail.com>
2025-11-26 14:46:54 -0300

    refatoring data model

--- backendnest/migrations/1759950210502_criar-tabela-posts.sql
+++ backendnest/migrations/1759950210502_criar-tabela-posts.sql
@@ -1,49 +1,66 @@
 -- Up Migration
 CREATE TABLE "producers" (
-  "id" UUID  PRIMARY KEY,
+  "id_producer" UUID  PRIMARY KEY DEFAULT gen_random_uuid(),
   "username" varchar NOT NULL,
   "cpf_or_cnpj" varchar UNIQUE NOT NULL,
   "hashed_password" varchar NOT NULL,
   "role" varchar NOT NULL DEFAULT 'USER',
-  "created_at" timestamp DEFAULT NOW()
+  "created_at" timestamp DEFAULT NOW(),
+  "updated_at" timestamp
 );
 
 CREATE TABLE "properties" (
-  "id" UUID PRIMARY KEY,
-  "producers_id" UUID NOT NULL,
+  "id_property" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  "id_producer" UUID NOT NULL,
   "city" varchar NOT NULL,
   "state" varchar NOT NULL,
   "total_area" float NOT NULL,
   "arable_area" float NOT NULL,
   "vegetation_area" float NOT NULL,
-  "created_at" timestamp DEFAULT NOW()
+  "created_at" timestamp DEFAULT NOW(),
+  "updated_at" timestamp,
+
+  CONSTRAINT fk_properties_producers
+    FOREIGN KEY ("id_producer")
+    REFERENCES "producers" ("id_producer")
+    ON DELETE CASCADE
 );
 
 CREATE TABLE "crops" (
-  "id" UUID PRIMARY KEY,
-  "properties_id" UUID NOT NULL,
+  "id_crop" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  "id_property" UUID NOT NULL,
   "name" varchar NOT NULL,
-  "created_at" timestamp DEFAULT NOW()
+  "created_at" timestamp DEFAULT NOW(),
+  "updated_at" timestamp,
+
+  CONSTRAINT fk_crops_properties
+    FOREIGN KEY ("id_property")
+    REFERENCES "properties" ("id_property")
+    ON DELETE CASCADE
 );
 
 CREATE TABLE "cultures" (
-  "id" UUID PRIMARY KEY,
-  "crops_id" UUID NOT NULL,
+  "id_culture" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  "id_crop" UUID NOT NULL,
   "name" varchar NOT NULL,
-  "created_at" timestamp DEFAULT NOW()
+  "created_at" timestamp DEFAULT NOW(),
+  "updated_at" timestamp,
+
+  CONSTRAINT fk_cultures_crops
+    FOREIGN KEY ("id_crop")
+    REFERENCES "crops" ("id_crop")
+    ON DELETE CASCADE
 );
 
-ALTER TABLE "properties" ADD FOREIGN KEY ("producers_id") REFERENCES "producers" ("id");
+ALTER TABLE "properties" ADD FOREIGN KEY ("id_producer") REFERENCES "producers" ("id_producer");
 
-ALTER TABLE "crops" ADD FOREIGN KEY ("properties_id") REFERENCES "properties" ("id");
+ALTER TABLE "crops" ADD FOREIGN KEY ("id_property") REFERENCES "properties" ("id_property");
 
-ALTER TABLE "cultures" ADD FOREIGN KEY ("crops_id") REFERENCES "crops" ("id");
+ALTER TABLE "cultures" ADD FOREIGN KEY ("id_crop") REFERENCES "crops" ("id_crop");
+
 -- Down Migration
 
-DROP TABLE "cultures";
-
-DROP TABLE "crops";
-
-DROP TABLE "properties";
-
-DROP TABLE "producers";
+DROP TABLE IF EXISTS "cultures";
+DROP TABLE IF EXISTS "crops";
+DROP TABLE IF EXISTS "properties";
+DROP TABLE IF EXISTS "producers";
