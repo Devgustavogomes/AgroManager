a1c83866cba1f463437a142cb3875c7674ce4800
Gustavo Gomes <devgustavogomes31@gmail.com>
2026-01-15 17:32:39 -0300

    chore: change type OwnerService decorator

--- backendnest/src/decorators/owner.decorator.ts
+++ backendnest/src/decorators/owner.decorator.ts
@@ -1,5 +1,5 @@
 import { SetMetadata } from '@nestjs/common';
 
 export const OWNER_SERVICE_KEY = 'ownerService';
-export const OwnerService = (service: any) =>
+export const OwnerService = (service: object) =>
   SetMetadata(OWNER_SERVICE_KEY, service);
