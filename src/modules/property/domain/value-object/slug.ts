2ded031f27df5cd462fa7401bab41e1ccf0bd684
Devgustavogomes <devgustavogomes31@gmail.com>
2026-05-19 14:39:17 -0300

    feat: create slug value object

---
+++ src/modules/property/domain/value-object/slug.ts
@@ -0,0 +1,21 @@
+export class Slug {
+  private constructor(private text: string) {}
+
+  static createFromText(text: string) {
+    const slugText = text
+      .normalize('NFKD')
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w\-]+/g, '')
+      .replace(/_/g, '-')
+      .replace(/--+/g, '-')
+      .replace(/-$/g, '');
+
+    return new Slug(slugText);
+  }
+
+  get getSlug() {
+    return this.text;
+  }
+}
