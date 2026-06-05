40b9a5afcd9c8712736f7670e6d05e76983b17e8
Devgustavogomes <devgustavogomes31@gmail.com>
2026-06-05 12:52:53 -0300

    feat: change architecture of culture

--- src/modules/culture/aplication/use-cases/is-culture-owner.service.ts
+++ src/modules/culture/aplication/use-cases/is-culture-owner.service.ts
@@ -6,12 +6,6 @@ export class IsCultureOwnerUseCase {
   constructor(private readonly cultureRepository: CultureContract) {}
 
   async execute(producerId: string, cultureId: string): Promise<boolean> {
-    const result = await this.cultureRepository.isOwner(producerId, cultureId);
-
-    if (!result) {
-      return false;
-    }
-
-    return true;
+    return await this.cultureRepository.isOwner(producerId, cultureId);
   }
 }
--- src/modules/property/application/use-cases/is-property-owner.ts
+++ src/modules/property/application/use-cases/is-property-owner.ts
@@ -6,15 +6,6 @@ export class IsPropertyOwnerUseCase {
   constructor(private readonly propertyRepository: PropertyContract) {}
 
   async execute(producerId: string, propertyId: string): Promise<boolean> {
-    const result = await this.propertyRepository.isOwner(
-      producerId,
-      propertyId,
-    );
-
-    if (!result) {
-      return false;
-    }
-
-    return true;
+    return await this.propertyRepository.isOwner(producerId, propertyId);
   }
 }
