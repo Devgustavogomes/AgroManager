1f047d37d3af96f3c8cdbbb2d27ff2ee7ef50be1
Devgustavogomes <devgustavogomes31@gmail.com>
2026-06-22 22:54:36 -0300

    chore: add domain events to base entity

--- api/src/shared/domain/entities/entity.ts
+++ api/src/shared/domain/entities/entity.ts
@@ -1,7 +1,17 @@
 export class Entity<Props> {
   protected props: Props;
 
+  protected domainEvents = [];
+
   protected constructor(props: Props) {
     this.props = props;
+  }
+
+  public getDomainEvents() {
+    return this.domainEvents;
+  }
+
+  public clearDomainEvents(): void {
+    this.domainEvents = [];
   }
 }
