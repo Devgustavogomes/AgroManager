782ee21771864fc2159e4c7324e96a8cb7cb7ce6
Devgustavogomes <devgustavogomes31@gmail.com>
2026-07-01 16:11:39 -0300

    fix: fix lint error

--- api/src/shared/filters/globalErrorHandler.ts
+++ api/src/shared/filters/globalErrorHandler.ts
@@ -1,13 +1,14 @@
 import { Catch, ArgumentsHost } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 import { BaseError } from '../domain/errors/baseError';
+import { Response } from 'express';
 
 @Catch()
 export class GlobalErrorHandler extends BaseExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
     if (exception instanceof BaseError) {
       const ctx = host.switchToHttp();
-      const response = ctx.getResponse();
+      const response = ctx.getResponse<Response>();
 
       const errorResponse = {
         statusCode: exception.statusCode,
