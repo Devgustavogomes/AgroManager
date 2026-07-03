7c690feae67cd27f7e95a75632f08d8d70e17e06
Devgustavogomes <devgustavogomes31@gmail.com>
2026-07-03 12:59:51 -0300

    feat: create notification entity

---
+++ api/src/shared/domain/entities/notification.entity.ts
@@ -0,0 +1,67 @@
+import { Optional } from 'src/shared/types/optional';
+
+interface NotificationProps {
+  notificationId: string;
+  event: string;
+  title: string;
+  content: string;
+  read: boolean;
+  createdAt: Date;
+  updatedAt: Date | null;
+}
+
+export class Notification {
+  protected props: NotificationProps;
+
+  private constructor(props: NotificationProps) {
+    this.props = props;
+  }
+
+  public static create(
+    props: Optional<
+      NotificationProps,
+      'notificationId' | 'read' | 'createdAt' | 'updatedAt'
+    >,
+  ) {
+    return new Notification({
+      ...props,
+      notificationId: props.notificationId ?? 'non-registered',
+      read: props.read ?? false,
+      createdAt: props.createdAt ?? new Date(),
+      updatedAt: props.updatedAt ?? null,
+    });
+  }
+
+  public readNotification() {
+    this.props.read = true;
+    this.props.updatedAt = new Date();
+  }
+
+  get notificationId() {
+    return this.props.notificationId;
+  }
+
+  get event() {
+    return this.props.event;
+  }
+
+  get title() {
+    return this.props.title;
+  }
+
+  get content() {
+    return this.props.content;
+  }
+
+  get read() {
+    return this.props.read;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+}
