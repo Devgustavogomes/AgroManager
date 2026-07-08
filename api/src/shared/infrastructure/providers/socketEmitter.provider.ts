bf592e8e6e1a427439ea39e718c598eb1a94c524
Devgustavogomes <devgustavogomes31@gmail.com>
2026-07-08 14:42:32 -0300

    fix: add @injectable decorator

--- api/src/shared/infrastructure/providers/socketEmitter.provider.ts
+++ api/src/shared/infrastructure/providers/socketEmitter.provider.ts
@@ -3,7 +3,9 @@ import {
   EventEmitterContract,
 } from 'src/shared/domain/providers/emitterProvider.contract';
 import { EventEmitter2 } from '@nestjs/event-emitter';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class EventEmitterProvider implements EventEmitterContract {
   constructor(private readonly eventEmitter: EventEmitter2) {}
   emit<T>(event: EmitterPayload<T>): void {
