0d420f40b1de6e1c858324816715c6fe0a5caf07
Devgustavogomes <devgustavogomes31@gmail.com>
2026-07-15 15:56:28 -0300

    fix: fix type of request in global erro handling

--- api/src/shared/infrastructure/filters/globalErrorHandler.ts
+++ api/src/shared/infrastructure/filters/globalErrorHandler.ts
@@ -1,6 +1,6 @@
 import { Catch, ArgumentsHost } from '@nestjs/common';
 import { BaseExceptionFilter, HttpAdapterHost } from '@nestjs/core';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { BaseError } from 'src/shared/domain/errors/baseError';
 import { PinoLogger } from 'nestjs-pino';
 import { AuthenticatedRequest } from 'src/shared/application/types/authenticatedRequest';
@@ -17,7 +17,7 @@ export class GlobalErrorHandler extends BaseExceptionFilter {
 
   catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
-    const request = ctx.getRequest<AuthenticatedRequest>();
+    const request = ctx.getRequest<AuthenticatedRequest | Request>();
 
     const isBusinessError =
       exception instanceof BaseError && exception.statusCode < 500;
@@ -26,7 +26,8 @@ export class GlobalErrorHandler extends BaseExceptionFilter {
       err: exception,
       path: request.url,
       method: request.method,
-      producerId: request.producer?.id ?? 'not authenticated',
+      producerId:
+        'producer' in request ? request.producer.id : 'not authenticated',
     };
 
     if (isBusinessError) {
