f81a4fbcd092ae90c69af8b566b8ec4a5753df9c
Devgustavogomes <devgustavogomes31@gmail.com>
2026-08-13 15:34:12 -0300

    feat: create api interceptor

--- web/src/services/api.service.ts
+++ web/src/services/api.service.ts
@@ -1,5 +1,21 @@
 import axios from "axios";
+import { tokenStorage } from "../store/tokenStorage";
 
 export const api = axios.create({
   baseURL: import.meta.env.BASE_URL,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = tokenStorage.getToken();
+
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
