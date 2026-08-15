3555456ea66068f407c26f7d974309a2d92ba07d
Devgustavogomes <devgustavogomes31@gmail.com>
2026-08-15 15:58:22 -0300

    feat: create login route

--- web/src/types/auth.type.ts
+++ web/src/types/auth.type.ts
@@ -1,7 +1,17 @@
-export interface LoginCredentials {
-  email: string;
-  password: string;
-}
+import z from "zod";
+
+export const loginSchema = z.object({
+  email: z
+    .email("Por favor, digite um formato de e-mail válido.")
+    .min(1, "O e-mail é obrigatório.")
+    .trim(),
+  password: z
+    .string()
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{8,36}$/)
+    .trim(),
+});
+
+export type LoginCredentials = z.infer<typeof loginSchema>;
 
 export interface AuthResponse {
   accessToken: string;
