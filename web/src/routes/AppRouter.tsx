b8757a96fa7aafaa31aa4f1d733aee7ade0b8bf6
Devgustavogomes <devgustavogomes31@gmail.com>
2026-08-21 13:22:48 -0300

    feat: configure main layout and app router

--- web/src/routes/AppRouter.tsx
+++ web/src/routes/AppRouter.tsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "../features/auth/pages/LoginPage";
 import { RegisterPage } from "../features/auth/pages/RegisterPage";
+import { MainLayout } from "../components/layout/MainLayout";
 
 export function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<h1> Página Inicial</h1>}></Route>
-      <Route path="/login" element={<LoginPage />}></Route>
-      <Route path="/register" element={<RegisterPage />}></Route>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<div className="max-w-7xl mx-auto px-4 py-8"><h1 className="text-2xl font-bold">Página Inicial</h1></div>}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
+      </Route>
     </Routes>
   );
 }
