4d8fec01d93354660e5c8c941e0ef7bea9a71d45
Devgustavogomes <devgustavogomes31@gmail.com>
2026-08-21 09:06:12 -0300

    feat: implement register page and update login page

--- web/src/routes/AppRouter.tsx
+++ web/src/routes/AppRouter.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "../features/auth/pages/LoginPage";
+import { RegisterPage } from "../features/auth/pages/RegisterPage";
 
 export function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<h1> Página Inicial</h1>}></Route>
       <Route path="/login" element={<LoginPage />}></Route>
+      <Route path="/register" element={<RegisterPage />}></Route>
     </Routes>
   );
 }
